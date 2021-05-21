3e89d19276aa180a3ba7c91166bafd44077331df
andochiwa <a1066079469@gmail.com>
2021-05-22 03:59:28 +0900

    multi return value

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("hello world")
+	//sum, sub := getSumAndSub(1, 2)
+}
+
+// 多个返回值
+func getSumAndSub(n1 int, n2 int) (int, int) {
+	sum := n1 + n2
+	sub := n1 - n2
+	return sum, sub
 }
