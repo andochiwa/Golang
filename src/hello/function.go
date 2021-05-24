76896d150a37223f637949346ede5f0e4300d945
andochiwa <a1066079469@gmail.com>
2021-05-25 02:44:54 +0900

    customize type by func

--- src/hello/function.go
+++ src/hello/function.go
@@ -11,14 +11,24 @@ func getSumFun(sum func(int, int) int, n1 int, n2 int) int {
 	return sum(n1, n2)
 }
 
+// 可以自定义类型为函数
+type myFunc func(int, int) int
+
+func getCustomizeSum(sum myFunc, n1 int, n2 int) int {
+	return sum(n1, n2)
+}
+
 func main() {
 	// 函数也是一种数据类型，变量可以赋值为函数
 	a := getSum
 	fmt.Printf("a类型为%T getSum类型为%T\n", a, getSum)
 	// getSum(10, 40)
 	sum := a(10, 40)
-	fmt.Printf("sum = %d\n", sum)
+	fmt.Printf("函数作为变量 sum = %d\n", sum)
 	// 通过传入函数类型形参
 	sum = getSumFun(a, 10, 50)
-	fmt.Println("sum =", sum)
+	fmt.Println("函数作为形参 sum =", sum)
+	// 通过自定义函数形参
+	sum = getCustomizeSum(a, 20, 70)
+	fmt.Println("自定义函数类型 sum =", sum)
 }
