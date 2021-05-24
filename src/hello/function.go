1802632860aa089f98522a78213a864aae92c909
andochiwa <a1066079469@gmail.com>
2021-05-25 02:50:48 +0900

    func names the return value

--- src/hello/function.go
+++ src/hello/function.go
@@ -18,6 +18,13 @@ func getCustomizeSum(sum myFunc, n1 int, n2 int) int {
 	return sum(n1, n2)
 }
 
+// 函数也支持对返回值命名
+func getSubAndPro(n1 int, n2 int) (sub int, pro int) {
+	sub = n1 - n2
+	pro = n1 * n2
+	return
+}
+
 func main() {
 	// 函数也是一种数据类型，变量可以赋值为函数
 	a := getSum
@@ -31,4 +38,7 @@ func main() {
 	// 通过自定义函数形参
 	sum = getCustomizeSum(a, 20, 70)
 	fmt.Println("自定义函数类型 sum =", sum)
+	// 函数对返回值命名
+	sub, pro := getSubAndPro(30, 10)
+	fmt.Println("函数对返回值命名 sub =", sub, "pro =", pro)
 }
