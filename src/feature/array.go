f224c55958b5362ea758ccb431656d6bf9b1be96
andochiwa <a1066079469@gmail.com>
2021-05-26 01:06:56 +0900

    go array type

---
+++ src/feature/array.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [3]int = [3]int{2, 3, 8}
+	arr[1] = 1
+	fmt.Println(arr)
+
+	var arr2 = [...]int{3, 4, 8}
+	fmt.Println(arr2)
+
+	var names = [...]string{1: "tom", 2: "jack", 0: "marry"}
+	fmt.Println(names)
+}
