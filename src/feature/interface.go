c6c000d341a7798935ecd386c87e5be165ee0ba7
andochiwa <a1066079469@gmail.com>
2021-05-29 04:42:51 +0900

    type assertion

--- src/feature/interface.go
+++ src/feature/interface.go
@@ -5,6 +5,7 @@ import "fmt"
 type (
 	Usb interface {
 		Start()
+		Stop()
 	}
 
 	// Phone 实现Usb接口
@@ -22,15 +23,32 @@ type (
 //只需要分别实现start()方法，即可实现Usb接口
 
 func (phone Phone) Start() {
-	fmt.Println("phone...")
+	fmt.Println("phone...Start")
+}
+
+func (phone Phone) Stop() {
+	fmt.Println("phone...Stop")
+}
+
+func (phone Phone) Call() {
+	fmt.Println("phone...Call")
 }
 
 func (camera Camera) Start() {
-	fmt.Println("Camera...")
+	fmt.Println("Camera...Start")
+}
+
+func (camera Camera) Stop() {
+	fmt.Println("Camera...Stop")
 }
 
 func (pc *Pc) Working(usb Usb) {
 	usb.Start()
+	// *********类型断言，如果是Phone类型就执行Call()************
+	if phone, phoneOk := usb.(Phone); phoneOk {
+		phone.Call()
+	}
+	usb.Stop()
 }
 
 func main() {
