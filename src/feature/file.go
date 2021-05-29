a64509a811b33033c9474cca507afd29025deb78
andochiwa <a1066079469@gmail.com>
2021-05-29 23:06:00 +0900

    read file

--- src/feature/file.go
+++ src/feature/file.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	// 打开文件
+	file, err := os.Open("src/file/test.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 输出文件
+	fmt.Println(file.Name())
+	// 关闭文件
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(file)
+	// 读取文件内容
+	reader := bufio.NewReader(file)
+	for {
+		str, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(str)
+	}
 }
