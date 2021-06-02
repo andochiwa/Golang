efda6dc7d96e48075109e6cf0e595c07c6698e06
andochiwa <a1066079469@gmail.com>
2021-06-03 00:27:42 +0900

    reflection to generic data type

--- src/feature/reflection.go
+++ src/feature/reflection.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
+func reflectTest(a interface{}) {
+	// 通过反射获取type，kind，值
+	// 获取Type
+	rType := reflect.TypeOf(a)
+	fmt.Println("rType =", rType)
+
+	// 获取Value
+	rValue := reflect.ValueOf(a)
+	fmt.Println("rValue =", rValue)
+}
+
 func main() {
+
+	num := 100
+	reflectTest(num)
 
 }
