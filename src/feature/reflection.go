c6cc6b2b16a2ed02df38cf7f5861c75795f17998
andochiwa <a1066079469@gmail.com>
2021-06-03 00:49:30 +0900

    reflection to struct

--- src/feature/reflection.go
+++ src/feature/reflection.go
@@ -5,6 +5,12 @@ import (
 	"reflect"
 )
 
+type Student2 struct {
+	Name  string
+	Age   int
+	Score int
+}
+
 func reflectTest(a interface{}) {
 	// 通过反射获取type，kind，值
 	// 获取Type
@@ -16,9 +22,23 @@ func reflectTest(a interface{}) {
 	fmt.Println("rValue =", rValue)
 }
 
+func reflectTest2(a interface{}) {
+	// 通过反射获取type，kind，值
+	// 获取Type
+	rType := reflect.TypeOf(a)
+	fmt.Printf("rType = %v type = %T\n", rType, rType)
+
+	// 获取Value
+	rValue := reflect.ValueOf(a)
+	fmt.Printf("rValue = %v, type = %T\n", rValue, rValue)
+}
+
 func main() {
 
 	num := 100
 	reflectTest(num)
+
+	student := Student2{"andochiwa", 10, 50}
+	reflectTest2(student)
 
 }
